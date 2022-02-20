35e1fd36054a7125a89ce2d167f9095f6c768001
David Hernando Caycedo Blum <davidxx55@gmail.com>
2022-02-19 21:58:32 -0500

    agregando comentario

--- HolaMundo/src/holaMundo.java
+++ HolaMundo/src/holaMundo.java
@@ -1,6 +1,6 @@
 
 public class holaMundo {
-
+	//*primer ejercicio del diplomado
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("!Hola Munod!");
