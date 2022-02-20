d036e7691303026c58e12fc71daecf29969b0e56
David Hernando Caycedo Blum <davidxx55@gmail.com>
2022-02-19 22:23:54 -0500

    Enviando msn desde una variable 2

--- HolaMundo/src/holaMundo.java
+++ HolaMundo/src/holaMundo.java
@@ -3,7 +3,11 @@ public class holaMundo {
 	//*primer ejercicio del diplomado
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("!Hola Munod!");
+		//msn directo 
+		System.out.println("!Hola Mundo!");
+		//con variable
+		String msn="!Hola Mundo des una variable!";
+		System.out.println(msn);	
 	}
 
 }
