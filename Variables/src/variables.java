03bb78b1913533f07951f0acdae1e102d41ceb23
David Hernando Caycedo Blum <davidxx55@gmail.com>
2022-02-22 12:43:44 -0500

    ejercicos de variav bles

---
+++ Variables/src/variables.java
@@ -0,0 +1,54 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// byte: Emplea un sólo byte (8 bits) de almacenamiento.
+		//Esto pernite almacenar valores entre [-128 , 32.767].
+		
+		byte numeroByte = 9;
+		
+		//short: Emplea el doble almacenamiento de (biyte).
+		//Esto permite almacenar valores entre [-32768. 32767].
+		
+		short numeroShort = 32767;
+		
+		//int: Emplea un tamaño mayot, 4 bytes (323 bits).
+		//Esto permite almacenar vaolres entre [-2147483648. 2147483647].
+		
+		int numeroInt = 41825;
+		
+		//long: Emplea el tamaño mayor de todos los enteros, 8 bytes (64 bits).
+		//Esto permite almacenar valores entre [-9223372036854775808. 9223372036854775807].
+		
+		long numeroLong = 926465464697266565L;
+		
+		//float: Emplea un tamaño de 32 bits (4 bytes).
+		//Esto permite almacenar valores entre [-3.4028234 E+8, 1.40239846E+45].
+		
+		float numeroFoalt = 5976464F;
+		
+		//double: Emplea un tamaño de 64 bits (8 bytes).
+		//Esto permite almacenar valores entre [-1.7976931348623157E+309. 4.94065645841246544E-324].
+		
+		double = numeroDouble = 2654792142478F;
+		
+		//boolean: Se emplea con la finalidad de trabajar con valores con valores verdaderos7false(booleanos).
+		//Se traducen sus valores en True/False.
+		
+		boolean variablesBoolean = true;
+		
+		//char: Se emplea para almacenar caracteres individuales (letras, aunque puede contener números).
+		//Utiliza 16 bits y se códifica sobre UTF-16 Unicode.
+		
+		char NumeroChar = 1;
+		char letraChar ='D';
+		
+		
+		
+		
+		
+
+	}
+
+}
