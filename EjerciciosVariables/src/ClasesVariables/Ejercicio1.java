6ddb9b6a9663d1cd02a3c64f007e9f444ed19d9c
David Hernando Caycedo Blum <davidxx55@gmail.com>
2022-02-22 19:21:27 -0500

    Ejercios del modulo 1 parte 1

---
+++ EjerciciosVariables/src/ClasesVariables/Ejercicio1.java
@@ -0,0 +1,31 @@
+package ClasesVariables;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Nombre de persona
+		String nombrePersona="David H Caycedo B.";
+		
+		//numero con dos decimalos
+		double numeroConDecimales= 4.23;
+		
+		//variable constatte 
+		
+		final double valorDolar=3.20;
+		
+		//pi
+		
+		float pi=3.1415926f;
+		
+		//euler
+		double eular=2.718281828459045;
+		
+		System.out.println(nombrePersona);
+		System.out.println(numeroConDecimales);
+		System.out.println(valorDolar);
+		System.out.println(pi);
+		System.out.println(eular);
+	}
+
+}
---
+++ EjerciciosVariables/src/ClasesVariables/Ejercicio2.java
@@ -0,0 +1,20 @@
+package ClasesVariables;
+
+public class Ejercicio2 {
+	double a=8.00;
+	char b='A';
+	String C="Colombia";
+	char d='A';
+	float e =74548;
+	int numero;
+	String nombtrePersona;
+	double edad;
+	String altura;
+	double alturaReal= Double.parseDouble(altura);
+	int numero2 = 6;
+	int  a1 =98;
+	int x=a1;
+	String y ="25";
+	String g=y;
+	
+}
